e4cfe9bd82072534a0caec2845924280fdf32a83
BrinzeiLucian <78175992+BrinzeiLucian@users.noreply.github.com>
2023-01-04 22:03:24 +0200

    users controller

---
+++ controllers/users.js
@@ -0,0 +1,69 @@
+//imports
+const express = require('express');
+let router = express.Router();
+const User = require('../models/User');
+const wrapAsync = require('../factory/wrapAsync');
+const passport = require('passport');
+const { session } = require('passport');
+const { checkReturnTo } = require('../factory/middleware');
+
+//controllers
+//render register page
+module.exports.renderRegisterPage = (req, res) => {
+    res.render('auth/register', { pageName: 'User Registration'});
+};
+
+//register user
+module.exports.registerUser = async(req, res, next) => {
+    try{
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+    if(err){return next(err);}
+    req.flash('success', 'Registered successfully !');
+    res.redirect('/locations');        
+    });
+    } catch(e){
+        req.flash("error", `${e.message}.`, "Please try again!");
+        res.redirect("/register");
+    };
+};
+
+//render login page
+module.exports.renderLoginPage = (req, res) => {
+    if(!req.isAuthenticated()){
+    res.render('auth/login', { pageName: 'User login'});
+    } else {
+        req.flash('success', 'Welcome back !');
+        return res.redirect('/locations');
+    }
+};
+
+module.exports.renderLogic = (req, res) => {
+    if(req.query.returnTo){
+        req.session.returnTo = req.query.returnTo;
+    }
+};
+
+//login
+module.exports.login = (req, res) => {
+    req.flash('success', 'Welcome back !');
+    const returnToUrl = res.locals.returnTo || '/locations';
+    res.redirect(returnToUrl);
+};
+
+//logout
+module.exports.logout = (req, res, next) => {
+    if(!req.isAuthenticated()){
+        req.flash('error', 'You must be signed in !');
+        return res.redirect('/login');
+    }
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      req.flash('success', 'Logged out successfully !');
+      res.redirect('/locations');
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -6,62 +6,20 @@ const wrapAsync = require('../factory/wrapAsync');
 const passport = require('passport');
 const { session } = require('passport');
 const { checkReturnTo } = require('../factory/middleware');
+const users = require('../controllers/users');
 
 //routes
-router.get('/register', (req, res) => {
-    res.render('auth/register', { pageName: 'User Registration'});
-});
+//register
+router.get('/register', users.renderRegisterPage);
+router.post('/register', wrapAsync(users.registerUser));
 
-router.post('/register', wrapAsync(async(req, res, next) => {
-    try{
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-    if(err){return next(err);}
-    req.flash('success', 'Registered successfully !');
-    res.redirect('/locations');        
-    });
-    } catch(e){
-        req.flash("error", `${e.message}.`, "Please try again!");
-        res.redirect("/register");
-    };
-}));
+//login
+router.get('/login', users.renderLoginPage);
+router.post('/renderLogin', users.renderLogic);
+router.post('/login', checkReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.get('/login', (req, res) => {
-    if(!req.isAuthenticated()){
-    res.render('auth/login', { pageName: 'User login'});
-    } else {
-        req.flash('success', 'Welcome back !');
-        return res.redirect('/locations');
-    }
-});
-
-router.post('/renderLogin', (req, res) => {
-    if(req.query.returnTo){
-        req.session.returnTo = req.query.returnTo;
-    }
-});
-
-router.post('/login', checkReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome back !');
-    const returnToUrl = res.locals.returnTo || '/locations';
-    res.redirect(returnToUrl);
-});
-
-router.get('/logout', (req, res, next) => {
-    if(!req.isAuthenticated()){
-        req.flash('error', 'You must be signed in !');
-        return res.redirect('/login');
-    }
-    req.logout(function (err) {
-      if (err) {
-        return next(err);
-      }
-      req.flash('success', 'Logged out successfully !');
-      res.redirect('/locations');
-    });
-});
+//logout
+router.get('/logout', users.logout);
 
 //export
 module.exports = router;
